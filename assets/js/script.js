25ca7696200a8ff07fbef580899005bc9ebc61b5
hishida0821 <hishida1204@gmail.com>
2023-12-08 16:22:03 +0900

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+'use strict';
+
+document.addEventListener("DOMContentLoaded", () => {
+
+  const jsonUrl = "/assets/json/list.json";
+  const dataList = document.getElementById("dataList")
+
+  fetch(jsonUrl)
+    .then(response => response.json())
+    .then(data => {
+      data.forEach(item => {
+        
+        let title = item.title;
+        let description = item.description;
+
+        let list = 
+          `<div class="list_item">` +
+          `<p class="list_title">` + title + `</p>` +
+          `<p class="list_text">` + description + `</p>`;
+
+        dataList.insertAdjacentHTML("beforeend", list);
+
+      });
+    })
+    .catch(error => {
+      console.error("jsonデータの取得に失敗しました。", error);
+    });
+});
